463ce2b7701c56404c4d395d47b432e26a47e5f5
ThiagoMedeiros21 <thfmadeiros01@gmail.com>
2025-01-15 17:00:35 -0300

    Menu ainda não funcional

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    printf("Projeto será iniciado em breve");
+    int option;
+    while(1){
+        printf("SEJA BEM VINDO AO BANCO BRASECO!!\nDIGITE:\n1-CRIAR CONTA\n2-EXCLUIR CONTA\n3-SACAR\n4-DEPOSITO,\n5-TRANSFERIR\n6-PAGAMENTO DA TAXA MENSAL\n7-SAIR ");
+        scanf("%d",&option);
+        if(option==1){
+            printf("...\n");
+        }
+        else if(option==2){
+            printf("...\n");
+        }
+        else if(option==3){
+            printf("...\n");
+        }
+        else if(option==4){
+            printf("...\n");
+        }
+        else if(option==5){
+            printf("...\n");
+        }
+        else if(option==6){
+            printf("...\n");
+        }
+        else if(option==7){
+            break;
+        }
+    }
     return 0;
 }
